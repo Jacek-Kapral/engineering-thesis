c1119b61cb57c0e9a57a3d16b86e2e4329f80106
jacek-kapral <jacek.kapral@proton.me>
2023-11-15 23:24:07 +0100

    Added more tables in init.sql, added printers.html with all printers view, added logic for viewing all printers

--- app.py
+++ app.py
@@ -14,13 +14,24 @@ def get_db_connection():
 def index():
     if request.method == 'POST':
         printer_serial_number = request.form['printer_serial_number']
+        counter_black = int(request.form['counter_black'])
+        counter_color = int(request.form['counter_color'])
         connection = get_db_connection()
         with connection.cursor() as cursor:
-            sql = "INSERT INTO printers(serial_number) VALUES (%s)"
-            cursor.execute(sql, (printer_serial_number,))
+            sql = "INSERT INTO printers(serial_number, black_counter, color_counter) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (printer_serial_number, counter_black, counter_color))
         connection.commit()
-        return 'Printer serial number ' + printer_serial_number + ' saved.'
+        return 'Printer data saved.'
     return render_template('index.html')
+
+@app.route('/printers')
+def printers():
+    connection = get_db_connection()
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM printers"
+        cursor.execute(sql)
+        printers = cursor.fetchall()
+    return render_template('printers.html', printers=printers)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- init.sql
+++ init.sql
@@ -1,7 +1,65 @@
 CREATE DATABASE IF NOT EXISTS mydb;
 USE mydb;
 
+CREATE TABLE IF NOT EXISTS clients (
+    tax_id INT NOT NULL PRIMARY KEY,
+    company VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    postal_code VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone INT NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS statuses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(255),
+    date DATE
+);
+
+CREATE TABLE IF NOT EXISTS contracts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    price_black DECIMAL(10,2),
+    price_color DECIMAL(10,2),
+    start_date DATE,
+    end_date DATE,
+    tax_id INT,
+    FOREIGN KEY (tax_id) REFERENCES clients(tax_id)
+);
+
 CREATE TABLE IF NOT EXISTS printers (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    serial_number VARCHAR(255) NOT NULL
+    serial_number VARCHAR(255) NOT NULL,
+    black_counter INT NOT NULL,
+    color_counter INT NOT NULL,
+    -- model VARCHAR(255) NOT NULL,
+    contracts_id INT,
+    tax_id INT,
+    statuses_id INT,
+    FOREIGN KEY (contracts_id) REFERENCES contracts(id),
+    FOREIGN KEY (tax_id) REFERENCES clients(tax_id),
+    FOREIGN KEY (statuses_id) REFERENCES statuses(id)
+);
+
+CREATE TABLE IF NOT EXISTS print_history (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    counter_black_history INT,
+    counter_color_history INT,
+    date DATE,
+    printers_id INT,
+    FOREIGN KEY (printers_id) REFERENCES printers(id)
+);
+
+CREATE TABLE IF NOT EXISTS models (
+    sn_prefix VARCHAR(255) PRIMARY KEY,
+    model VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    admin BOOLEAN NOT NULL,
+    email VARCHAR(255),
+    name VARCHAR(255) NOT NULL
 );
